3f30e08b02a5ee803b870def378918eb3c5d8222
Rodrigo Talin <161965166+RodrigoTalin@users.noreply.github.com>
2025-03-19 17:12:26 +0000

    VariaveisPratica

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,13 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+nome = 'Rodrigo'
+telefone = "31993912358"
 # Declare variáveis de cada tipo numérico
-
+numero_inteiro = 9
+numero = 1212.0
+numero1 = 1010.1
 
 # Declare uma variável do tipo booleano
+Verificacao = True
+Tipo = False
